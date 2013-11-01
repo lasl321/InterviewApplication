f897413b32fb32a67922d92d377d8c7b2b0a92c4
Luis Sotomayor <LSotomayor@1800dentist.com>
2013-10-31 17:25:21 -0700

    Initial commit. Again.

---
+++ InterviewApplication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace InterviewApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HandleForm()
+        {
+            var username = Request["Username"];
+            var password = Request["Password"];
+
+            Trace.WriteLine("username: " + username);
+            Trace.WriteLine("password: " + password);
+
+            if (username == "valid" && password == "valid")
+            {
+                return RedirectToAction("LoggedIn");
+            }
+
+            return RedirectToAction("LoginFailure");
+        }
+
+        public ActionResult LoggedIn()
+        {
+            return View();
+        }
+
+        public ActionResult LoginFailure()
+        {
+            return View();
+        }
+    }
+}
